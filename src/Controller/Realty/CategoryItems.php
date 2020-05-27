3beb915806470d1d736d5fda6059a0b3825a4eed
VS <slavyanrts@mail.ru>
2020-05-28 00:49:21 +0400

    построение меню объектов

--- src/Controller/Realty/CategoryItems.php
+++ src/Controller/Realty/CategoryItems.php
@@ -11,7 +11,6 @@ use App\Entity\Realty\Property;
 use App\Entity\Realty\Type;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryItems extends AbstractController
--- src/Services/Menu/MenuBuilder.php
+++ src/Services/Menu/MenuBuilder.php
@@ -5,28 +5,64 @@ declare(strict_types=1);
 
 namespace App\Services\Menu;
 
+use App\Entity\Realty\Category;
+use App\Entity\Realty\Type;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Menu\FactoryInterface;
 
 class MenuBuilder
 {
     private $factory;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
 
     /**
      * @param FactoryInterface $factory
      *
      * Add any other dependency you need
      */
-    public function __construct(FactoryInterface $factory)
+    public function __construct(FactoryInterface $factory, EntityManagerInterface $em)
     {
         $this->factory = $factory;
+        $this->em = $em;
     }
 
-    public function createMainMenu(array $options)
+    public function createMainMenu()
     {
         $menu = $this->factory->createItem('root');
+        $menu->addChild('Home', [
+            'uri' => '/'
+        ]);
 
-        $menu->addChild('Home', ['route' => 'homepage']);
+        $this->upItemFromType($menu);
 
         return $menu;
+    }
+
+    public function upItemFromType($menu){
+        $typeRepository = $this->em->getRepository(Type::class);
+        $types = $typeRepository->findAll();
+
+        foreach ($types as $type){
+            $upMenuItem = $menu->addChild($type->getTypeName());
+            $this->childItemFromCategory($upMenuItem, $type->getAlias());
+        }
+    }
+
+    public function childItemFromCategory($upMenuItem, $typeAlias)
+    {
+        $categoryRepository = $this->em->getRepository(Category::class);
+        $categories = $categoryRepository->findAll();
+        foreach($categories as $category){
+            $upMenuItem->addChild($category->getCategoryName(), [
+               'route' => 'category_items',
+                'routeParameters' => [
+                    'type' => $typeAlias,
+                    'alias' => $category->getAlias()
+                ]
+            ]);
+        }
     }
 }
